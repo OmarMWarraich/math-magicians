d819a23166880222c2b1dbcfd630ce77ac0b0b57
OmarMWarraich <earthling986@gmail.com>
2022-09-22 11:38:47 +0500

    feat: refactor to hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
-}
-
+const App = () => (
+  <div>
+    <Calculator />
+  </div>
+);
 export default App;
