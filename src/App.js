ffdb26c27178e30be2baaeff58df4ee39bb5179e
OmarMWarraich <earthling986@gmail.com>
2022-09-28 13:10:49 +0500

    feat:Add router and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Quotes from './pages/Quotes';
+import Welcome from './pages/Welcome';
 
 const App = () => (
-  <div>
-    <Calculator />
-  </div>
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quotes />} />
+    </Routes>
+  </>
 );
 export default App;
