056f24dca0f64882902f6ffd8573362db0e1d9e5
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-09-28 20:12:58 +0430

    Import the calculate file from the proper path

--- src/components/calculate.test.js
+++ src/components/calculate.test.js
@@ -1,54 +1,54 @@
 // write unit tests for calculate.js
 
-import calculate from './calculate';
+import calculate from '../logic/calculate';
 
 // test for AC button
 
 test('AC button', () => {
-    const obj = {
-        total: 5,
-        next: 5,
-        operation: '+',
-    };
-    const buttonName = 'AC';
-    const result = calculate(obj, buttonName);
-    expect(result).toEqual({
-        total: null,
-        next: null,
-        operation: null,
-    });
+  const obj = {
+    total: 5,
+    next: 5,
+    operation: '+',
+  };
+  const buttonName = 'AC';
+  const result = calculate(obj, buttonName);
+  expect(result).toEqual({
+    total: null,
+    next: null,
+    operation: null,
+  });
 });
 
 // test for equal button
 
 test('equal button', () => {
-    const obj = {
-        total: 5,
-        next: 5,
-        operation: '+',
-    };
-    const buttonName = '=';
-    const result = calculate(obj, buttonName);
-    expect(result).toEqual({
-        total: "10",
-        next: null,
-        operation: null,
-    });
+  const obj = {
+    total: 5,
+    next: 5,
+    operation: '+',
+  };
+  const buttonName = '=';
+  const result = calculate(obj, buttonName);
+  expect(result).toEqual({
+    total: '10',
+    next: null,
+    operation: null,
+  });
 });
 
 // test for number button
 
 test('number button', () => {
-    const obj = {
-        total: 5,
-        next: 5,
-        operation: '+',
-    };
-    const buttonName = '5';
-    const result = calculate(obj, buttonName);
-    expect(result).toEqual({
-        total: 5,
-        next: "55",
-        operation: '+',
-    });
+  const obj = {
+    total: 5,
+    next: 5,
+    operation: '+',
+  };
+  const buttonName = '5';
+  const result = calculate(obj, buttonName);
+  expect(result).toEqual({
+    total: 5,
+    next: '55',
+    operation: '+',
+  });
 });
