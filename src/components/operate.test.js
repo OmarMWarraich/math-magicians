eae70d9be924ba46c9db94546e3bea6394a9bccc
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-09-28 20:21:33 +0430

    Add unit test to test the operate function of the calculator

---
+++ src/components/operate.test.js
@@ -0,0 +1,19 @@
+import operate from '../logic/operate';
+
+describe('operate', () => {
+  it('should return 5 when 2 and 3 are passed', () => {
+    expect(operate(2, 3, '+')).toEqual('5');
+  });
+  it('should return -1 when 2 and 3 are passed', () => {
+    expect(operate(2, 3, '-')).toEqual('-1');
+  });
+  it('should return 6 when 2 and 3 are passed', () => {
+    expect(operate(2, 3, 'x')).toEqual('6');
+  });
+  it('should return 0.67 when 2 and 3 are passed', () => {
+    expect(operate(2, 3, '÷')).toEqual('0.67');
+  });
+  it('should return 2 when 2 and 3 are passed', () => {
+    expect(operate(2, 3, '%')).toEqual('2');
+  });
+});
